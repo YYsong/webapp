7f541abb87be277c864181231c1c98f6289f9c30
YYsong <songyayong@sina.com>
2014-11-20 13:03:00 +0800

    Revert "second"
    This reverts commit ea2174013a6313ab1a09cc6064188707ec089055.

--- song_2048/support2048.js
+++ song_2048/support2048.js
@@ -34,61 +34,11 @@ function getNumberColor( number ){
 }
 
 function nospace(board){
-	for(var i=0;i<num;i++)
-		for(var j=0;j<num;j++)
+	for(var i=0;i<4;i++)
+		for(var j=0;j<4;j++)
 			if(board[i][j]==0)
 				return false;
 				
 	return true;
 }
 
-function canMoveLeft(board){
-	for(var i=0;i<num;i++)
-		for(var j=1;j<num;j++)//start from 1 col
-			if(board[i][j] != 0)
-				if(board[i][j-1]==0||board[i][j-1]==board[i][j])return true;
-				
-	
-	return false;
-}
-function canMoveRight(board){
-	for(var i=0;i<num;i++)
-		for(var j=num-2;j>=0;j--)//start from n-2 col
-			if(board[i][j] != 0)
-				if(board[i][j+1]==0||board[i][j+1]==board[i][j])return true;
-				
-	
-	return false;
-}
-
-function canMoveUp(board){
-	for(var j=0;j<num;j++)
-		for(var i=1;i<num;i++)//start from 1 row
-			if(board[i][j] != 0)
-				if(board[i-1][j]==0||board[i-1][j]==board[i][j])return true;
-				
-	
-	return false;
-}
-
-function canMoveDown(board){
-	for(var j=0;j<num;j++)
-		for(var i=num-2;i>=0;i--)//start from n-2 col
-			if(board[i][j] != 0)
-				if(board[i+1][j]==0||board[i+1][j]==board[i][j])return true;
-				
-	
-	return false;
-}
-
-function noBlockHorizontal(row,col1,col2,board){
-	for(var i=col1+1;i<col2;i++)
-		if(board[row][i]!=0)return false;
-	return true;
-}
-
-function noBlockVertical(col,row1,row2,board){
-	for(var i=row1+1;i<row2;i++)
-		if(board[i][col]!=0)return false;
-	return true;
-}
